a8350dd0637c42d324e56e04cd0b51406cb7ce3a
Micah Cooper <micah+github@mrmicahcooper.com>
2018-11-19 21:12:50 -0600

    Initial commit

---
+++ lib/ecto_jsonapi.ex
@@ -0,0 +1,18 @@
+defmodule EctoJsonapi do
+  @moduledoc """
+  Documentation for EctoJsonapi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoJsonapi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_jsonapi_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoJsonapiTest do
+  use ExUnit.Case
+  doctest EctoJsonapi
+
+  test "greets the world" do
+    assert EctoJsonapi.hello() == :world
+  end
+end
