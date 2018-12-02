7fa368d29dee7d35b2f04eb76f8c171bfdfed348
Micah Cooper <micah+github@mrmicahcooper.com>
2018-12-02 14:23:46 -0600

    Convert 1 or many schema attributes and ids

--- lib/ecto_jsonapi.ex
+++ lib/ecto_jsonapi.ex
@@ -1,4 +1,28 @@
 defmodule EctoJsonapi do
+  def to_json(schemas) when is_list(schemas) do
+    %{
+      data: Enum.map(schemas, &data/1)
+    }
+  end
+
   def to_json(schema) do
+    %{
+      data: data(schema)
+    }
+  end
+
+  defp data(schema) do
+    %{
+      id: id(schema),
+      attributes: attributes(schema)
+    }
+  end
+
+  defp id(schema) do
+    schema.id
+  end
+
+  defp attributes(schema) do
+    Map.drop(schema, [:__meta__, :__struct__])
   end
 end
--- test/ecto_jsonapi_test.exs
+++ test/ecto_jsonapi_test.exs
@@ -1,18 +1,39 @@
-defmodule Ecto_JsonapiTest do
+defmodule EctoJsonapiTest do
   use ExUnit.Case, async: true
 
   setup do
-    credit_card = %CreditCard{
-      number: "2222333344445555",
-      expiration_date: "2018-05",
-      cvv: "123"
+    event = %Event{
+      id: 123,
+      name: "foo",
+      content: %{foo: "bar"}
     }
 
-    {:ok, credit_card}
+    {:ok, %{event: event}}
   end
 
   describe "to_json" do
-    test "with a single schema" do
+    test "1 schema with no associations", data do
+      json = EctoJsonapi.to_json(data.event)
+
+      assert get_in(json, [:data, :attributes, :name])
+      assert get_in(json, [:data, :attributes, :content])
+      assert get_in(json, [:data, :id])
+      refute get_in(json, [:data, :relationships])
+      refute get_in(json, [:data, :included])
+    end
+
+    test "2 schemas with no associations", data do
+      json = EctoJsonapi.to_json([data.event, data.event])
+
+      assert get_in(json.data, [Access.all(), :attributes, :name]) == ["foo", "foo"]
+
+      assert get_in(json.data, [Access.all(), :attributes, :content]) == [
+               %{foo: "bar"},
+               %{foo: "bar"}
+             ]
+
+      assert get_in(json.data, [Access.all(), :relationships]) == [nil, nil]
+      assert get_in(json.data, [Access.all(), :included]) == [nil, nil]
     end
   end
 end
