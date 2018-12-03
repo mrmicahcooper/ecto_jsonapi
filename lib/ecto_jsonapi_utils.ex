c56f66e24a0a18c46fe4bf0d11e714d5ac53ade4
Micah Cooper <micah+github@mrmicahcooper.com>
2018-12-02 20:05:48 -0600

    add `has_id?` to Utils:W

--- lib/ecto_jsonapi_utils.ex
+++ lib/ecto_jsonapi_utils.ex
@@ -1,4 +1,25 @@
 defmodule EctoJsonapi.Utils do
+  def has_id?(%{"data" => []}, _) do
+    false
+  end
+
+  def has_id?(%{"data" => data}, ids) when is_list(data) and is_list(ids) do
+    ids -- get_in(data, [Access.all(), "id"]) == []
+  end
+
+  def has_id?(%{"data" => data}, id) when is_list(data) do
+    ids = get_in(data, [Access.all(), "id"])
+    id in ids
+  end
+
+  def has_id?(%{"data" => data}, id) do
+    get_in(data, ["id"]) == id
+  end
+
+  def has_id?(_, _) do
+    false
+  end
+
   def has_type?(%{"data" => []}, _) do
     false
   end
--- test/ecto_jsonapi_utils_test.exs
+++ test/ecto_jsonapi_utils_test.exs
@@ -12,6 +12,21 @@ defmodule EctoJsonapi.UtilsUtilsTest do
      }}
   end
 
+  describe "has_id?" do
+    test "with no data", %{empty_data: json} do
+      assert EctoJsonapi.Utils.has_id?(json, "1") == false
+    end
+
+    test "with one data element", %{one_data: json} do
+      assert EctoJsonapi.Utils.has_id?(json, "1") == true
+    end
+
+    test "with multiple data elements", %{multiple_data: json} do
+      assert EctoJsonapi.Utils.has_id?(json, "1") == true
+      assert EctoJsonapi.Utils.has_id?(json, ["1"]) == true
+    end
+  end
+
   describe "has_type?" do
     test "with no data", %{empty_data: json} do
       assert EctoJsonapi.Utils.has_type?(json, "articles") == false
