817201cb8e740bd79206ae0ee510f13a2241bfaa
Micah Cooper <micah+github@mrmicahcooper.com>
2019-01-02 17:35:30 -0600

    Add belongs_to associations as included data

--- lib/ecto_jsonapi.ex
+++ lib/ecto_jsonapi.ex
@@ -6,7 +6,11 @@ defmodule EctoJsonapi do
   end
 
   def to_json(schema) do
-    %{data: data(schema)}
+    %{
+      data: data(schema),
+      included: included(schema),
+      meta: %{}
+    }
   end
 
   defp data(schema) do
@@ -17,12 +21,9 @@ defmodule EctoJsonapi do
       relationships: %{}
     }
 
-    {default_data, schema}
+    schema_map = Map.from_struct(schema)
 
-    {data, _schema} =
-      schema
-      |> Map.from_struct()
-      |> Enum.reduce({default_data, schema}, &data/2)
+    {data, _schema} = Enum.reduce(schema_map, {default_data, schema}, &data/2)
 
     data
   end
@@ -30,29 +31,50 @@ defmodule EctoJsonapi do
   defp type(%{__meta__: %{source: source}}), do: source
   defp type(ecto_schema_module), do: ecto_schema_module.__schema__(:source)
 
-  defp data({:id, id}, {doc, schema}) do
-    {Map.put(doc, :id, id), schema}
-  end
-
+  defp data({:id, id}, {doc, schema}), do: {Map.put(doc, :id, id), schema}
   defp data({:__meta__, _}, acc), do: acc
   defp data({:__struct__, _}, acc), do: acc
 
   defp data({key, %Ecto.Association.NotLoaded{}}, {doc, schema}) do
-    association = schema.__struct__.__schema__(:association, key)
-    type = type(association.queryable)
-    owner_key = association.owner_key
+    relationship = relationship(schema, key)
+    {put_in(doc, [:relationships, key], relationship), schema}
+  end
 
-    relationship = %{
-      data: %{
-        type: type,
-        id: Map.get(schema, owner_key)
-      }
-    }
-
+  # only add the relationship part of a associated schema. The `included` part is added later
+  defp data({key, %{__struct__: _}}, {doc, schema}) do
+    relationship = relationship(schema, key)
     {put_in(doc, [:relationships, key], relationship), schema}
   end
 
   defp data({key, value}, {doc, schema}) do
     {put_in(doc, [:attributes, key], value), schema}
+  end
+
+  defp included(schema) do
+    associations = schema.__struct__.__schema__(:associations)
+    Enum.reduce(associations, {[], schema}, &included_data/2)
+  end
+
+  defp included_data(association, {list, schema}) when not is_list(association) do
+    associated_data = Map.get(schema, association)
+
+    if associated_data.__struct__ == Ecto.Association.NotLoaded do
+      list
+    else
+      [data(associated_data) | list]
+    end
+  end
+
+  defp relationship(schema, key) do
+    association = schema.__struct__.__schema__(:association, key)
+    type = type(association.queryable)
+    owner_key = association.owner_key
+
+    %{
+      data: %{
+        type: type,
+        id: Map.get(schema, owner_key)
+      }
+    }
   end
 end
--- test/ecto_jsonapi_test.exs
+++ test/ecto_jsonapi_test.exs
@@ -2,20 +2,41 @@ defmodule EctoJsonapiTest do
   use ExUnit.Case, async: true
 
   setup do
+    event = %Event{
+      id: 123,
+      name: "foo",
+      content: %{foo: "bar"}
+    }
+
+    credit_card = %CreditCard{
+      id: 456,
+      number: "4444 4444 4444 4444",
+      expiration_date: "2018-02",
+      cvv: "123",
+      user_id: 123
+    }
+
+    user = %User{
+      id: 456,
+      name: "Micah Cooper",
+      email: "mrmicahcooper@gmail.com"
+    }
+
+    credit_card_with_user = %CreditCard{
+      id: 456,
+      number: "4444 4444 4444 4444",
+      expiration_date: "2018-02",
+      cvv: "123",
+      user_id: user.id,
+      user: user
+    }
+
     {:ok,
      %{
-       event: %Event{
-         id: 123,
-         name: "foo",
-         content: %{foo: "bar"}
-       },
-       credit_card: %CreditCard{
-         id: 456,
-         number: "4444 4444 4444 4444",
-         expiration_date: "2018-02",
-         cvv: "123",
-         user_id: 123
-       }
+       user: user,
+       event: event,
+       credit_card: credit_card,
+       credit_card_with_user: credit_card_with_user
      }}
   end
 
@@ -53,6 +74,20 @@ defmodule EctoJsonapiTest do
                  id: 123
                }
              }
+    end
+
+    test "1 schema with a loaded belongs to", data do
+      json = EctoJsonapi.to_json(data.credit_card_with_user)
+
+      assert json.data.relationships.user == %{
+               data: %{
+                 type: "users",
+                 id: data.user.id
+               }
+             }
+
+      assert get_in(json.included, [Access.all(), :id]) == [data.user.id]
+      assert get_in(json.included, [Access.all(), :attributes, :name]) == [data.user.name]
     end
   end
 end
