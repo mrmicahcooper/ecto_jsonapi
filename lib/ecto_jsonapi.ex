7ec291a57702ffe26c718447993b60f9f6c8d01c
Micah Cooper <micah+github@mrmicahcooper.com>
2019-02-10 16:54:10 -0600

    Manually delegate EctoJsonapi.Dump.dump

--- lib/ecto_jsonapi.ex
+++ lib/ecto_jsonapi.ex
@@ -1,4 +1,6 @@
 defmodule EctoJsonapi do
   def load(ectos) when is_list(ectos), do: EctoJsonapi.Load.load(ectos)
   def load(ecto, options \\ []), do: EctoJsonapi.Load.load(ecto, options)
+
+  def dump(json), do: EctoJsonapi.Dump.dump(json)
 end
