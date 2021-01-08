31e9e5569c6b108d2f8693a487710ade9335c790
Micah Cooper <micah+github@mrmicahcooper.com>
2021-01-08 14:53:06 -0500

    Document attributes/2

--- lib/ecto_jsonapi.ex
+++ lib/ecto_jsonapi.ex
@@ -3,6 +3,16 @@ defmodule EctoJsonapi do
   defdelegate load(ecto, options), to: EctoJsonapi.Load
   defdelegate dump(json), to: EctoJsonapi.Dump
 
+
+
+  @spec attributes(Eco.Schema, [atom]) :: map
+  @doc """
+  Return the selected attributes
+
+  This function also:
+  - Returns all the keys as strings
+  - Converts any underscores (`_`) in the keys to dashes (`-`).
+  """
   def attributes(schema, attributes) do
     for {key, value} <- Map.take(schema, attributes), into: %{} do
       key = to_string(key) |> String.replace("_", "-")
