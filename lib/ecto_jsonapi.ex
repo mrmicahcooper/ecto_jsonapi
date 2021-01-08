ba881b37cbdc829e0d522403dfbdc18c524f422a
Micah Cooper <micah+github@mrmicahcooper.com>
2021-01-08 14:50:06 -0500

    Add attributes/2

--- lib/ecto_jsonapi.ex
+++ lib/ecto_jsonapi.ex
@@ -2,4 +2,12 @@ defmodule EctoJsonapi do
   defdelegate load(ectos), to: EctoJsonapi.Load
   defdelegate load(ecto, options), to: EctoJsonapi.Load
   defdelegate dump(json), to: EctoJsonapi.Dump
+
+  def attributes(schema, attributes) do
+    for {key, value} <- Map.take(schema, attributes), into: %{} do
+      key = to_string(key) |> String.replace("_", "-")
+      {key, value}
+    end
+  end
+
 end
